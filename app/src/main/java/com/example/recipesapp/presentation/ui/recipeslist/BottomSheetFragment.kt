655ac3674466c577a5d380bcf1b78d8393cf7797
Almaz Kurmankulov <kurmankulov.it@gmail.com>
2021-04-21 03:06:11 +0700

    Fix BottomSheet

--- app/src/main/java/com/example/recipesapp/presentation/ui/recipeslist/BottomSheetFragment.kt
+++ app/src/main/java/com/example/recipesapp/presentation/ui/recipeslist/BottomSheetFragment.kt
@@ -10,11 +10,22 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
+
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
 class BottomSheetFragment() : BottomSheetDialogFragment() {
 
     private val viewModel: RecipeListViewModel by activityViewModels()
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        val binding = BottomSheetBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
